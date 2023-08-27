51fe80f237e39cc85cf52348a4a6684fe93c09b4
Fares-Fuad <fares.fuad@maf.ae>
2023-08-28 01:32:32 +0300

    Add clerk setup light dark mode

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-	return <div className="bg-slate-600">{children}</div>;
+	return <div className=" h-screen flex justify-center items-center">{children}</div>;
 };
 
 export default AuthLayout;
